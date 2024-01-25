6c43e714dab8bdf6dfb83f396093b273656cc647
anbaptista <153135932+anbaptista@users.noreply.github.com>
2024-01-25 14:02:29 +0000

    Borda adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
-     <h2>Rick Sanchez</h2>>
+      <div className="card">
+         <h2>Rick Sanchez</h2>>
      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
      </>
